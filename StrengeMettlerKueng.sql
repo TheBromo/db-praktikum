93bdaabda9e83efb056e0d14469b9c5ff79cfa49
Manuel <manuel@strenge.ch>
2023-12-11 17:24:51 +0000

    Update database constraints and insert data

--- StrengeMettlerKueng.sql
+++ StrengeMettlerKueng.sql
@@ -29,7 +29,7 @@ CREATE TABLE Spieler (
 -- Trainer Table 
 CREATE TABLE Trainer (
     Lizenz VARCHAR(50) NOT NULL,
-    Lizenzjahr INT NOT NULL CHECK (Lizenzjahr >= 1000 AND Lizenzjahr <= 9999)
+    Lizenzjahr INT NOT NULL CHECK (Lizenzjahr >= 0 AND Lizenzjahr <= 9999)
 )      INHERITS(Sportler);
 
 -- Spiel Table
@@ -44,18 +44,18 @@ CREATE TABLE Spiel (
 CREATE TABLE Mannschaft (
     MID INT PRIMARY KEY,
     Name VARCHAR(50) NOT NULL,
-    Gruendungsjahr INT NOT NULL CHECK (Gruendungsjahr >= 1000 AND Gruendungsjahr <= 9999)
+    Gruendungsjahr INT NOT NULL CHECK (Gruendungsjahr >= 0 AND Gruendungsjahr <= 9999)
 );
 
 -- Transfers Table 
 CREATE TABLE Transfers (
     TID INT PRIMARY KEY,
     SID INT NOT NULL,
-    Von INT NOT NULL, --TODO muss vorkommen in Manschaftstabelle
-    Zu INT NOT NULL, --TODO muss vorkommen in Manschaftstabelle
+    Von INT NOT NULL,
+    Zu INT NOT NULL, 
     Transferdatum DATE NOT NULL,
     Abloese INT NOT NULL,
-    FOREIGN KEY (Von) REFERENCES Mannschaft(MID),-- ist das korrekt so keine Ahnung :(
+    FOREIGN KEY (Von) REFERENCES Mannschaft(MID),
     FOREIGN KEY (Zu) REFERENCES Mannschaft(MID),
     FOREIGN KEY (SID) REFERENCES Sportler(SID)
 );
@@ -65,17 +65,18 @@ CREATE TABLE Trainiert (
     SID INT NOT NULL,
     MID INT NOT NULL,
     Gehalt INT NOT NULL,
-    PRIMARY KEY (SID),
+    PRIMARY KEY (SID, MID),
     FOREIGN KEY (SID) REFERENCES Sportler(SID),
     FOREIGN KEY (MID) REFERENCES Mannschaft(MID)
 );
+
 
 -- Gehoertzu Table
 CREATE TABLE Gehoertzu (
     SID INT NOT NULL,
     MID INT NOT NULL ,
     Verletzt BOOLEAN NOT NULL,
-    PRIMARY KEY (SID, MID),
+    PRIMARY KEY (SID),
     FOREIGN KEY (SID) REFERENCES Sportler(SID),
     FOREIGN KEY (MID) REFERENCES Mannschaft(MID)
 );
@@ -93,14 +94,6 @@ CREATE TABLE Spielt (
 -- Testdaten 
 -- ----------------------------------------------------------------------
 
--- Sportler Table
-INSERT INTO Sportler (SID, Vorname, Nachname, Geburtsdatum)
-VALUES (1, 'John', 'Doe', '1990-01-01'),
-       (2, 'Jane', 'Smith', '1995-05-10'),
-       (3, 'Michael', 'Johnson', '1988-09-15'),
-       (4, 'Emily', 'Brown', '1992-03-20'),
-       (5, 'David', 'Wilson', '1997-07-25');
-
 -- Spieler Table
 INSERT INTO Spieler (SID, Vorname, Nachname, Geburtsdatum, Sportart, Position, Gehalt)
 VALUES (1,'Amber', 'Smith', '1966-04-26', 'Football', 'Quarterback', 1000000),
@@ -110,12 +103,28 @@ VALUES (1,'Amber', 'Smith', '1966-04-26', 'Football', 'Quarterback', 1000000),
        (5,'Teresa', 'Williams', '1968-05-06', 'Tennis', 'Singles', 300000);
 
 -- Trainer Table
-INSERT INTO Trainer (SID, Vorname, Nachname, Geburtsdatum,Lizenz, Lizenzjahr)
+INSERT INTO Trainer (SID, Vorname, Nachname, Geburtsdatum,Lizenz, Lizenzjahr) --changed
 VALUES (6,'Tamara', 'Wood', '1996-05-30', 'Football Coach', 2010),
        (7,'Erin', 'Jones', '1958-07-30', 'Basketball Coach', 2015),
        (8,'Tammy', 'Phillips', '1968-08-08', 'Soccer Coach', 2005),
        (9,'Robin', 'Jackson', '1996-06-07', 'Baseball Coach', 2008),
        (10,'Craig', 'Thomas', '1973-07-05', 'Tennis Coach', 2012);
+
+-- Einfügen von Spielern in die Sportler-Tabelle
+INSERT INTO Sportler (SID, Vorname, Nachname, Geburtsdatum) VALUES --changed
+(1, 'Amber', 'Smith', '1966-04-26'),
+(2, 'Denise', 'Brennan', '1950-02-06'),
+(3, 'John', 'Parker', '1992-12-06'),
+(4, 'Larry', 'Ramos', '1966-08-31'),
+(5, 'Teresa', 'Williams', '1968-05-06');
+
+-- Einfügen von Trainern in die Sportler-Tabelle
+INSERT INTO Sportler (SID, Vorname, Nachname, Geburtsdatum) VALUES --changed
+(6, 'Tamara', 'Wood', '1996-05-30'),
+(7, 'Erin', 'Jones', '1958-07-30'),
+(8, 'Tammy', 'Phillips', '1968-08-08'),
+(9, 'Robin', 'Jackson', '1996-06-07'),
+(10, 'Craig', 'Thomas', '1973-07-05');
 
 -- Spiel Table
 INSERT INTO Spiel (SPID, Ort, Dauer, Datum)
@@ -134,20 +143,20 @@ VALUES (1, 'Team A', 2000),
        (5, 'Team E', 2015);
 
 -- Transfers Table
-INSERT INTO Transfers (TID, SID, Von, Zu, Transferdatum, Abloese)
-VALUES (1, 1, 1, 2, '2022-01-01', 500000),
-       (2, 2, 3, 1, '2022-02-05', 800000),
-       (3, 3, 4, 3, '2022-03-15', 700000),
-       (4, 4, 5, 4, '2022-04-10', 300000),
-       (5, 5, 2, 5, '2022-05-20', 400000);
+INSERT INTO Transfers (TID, SID, Von, Zu, Transferdatum, Abloese) --changed
+VALUES (1, 1, 1, 2, '2017-01-01', 500000),
+       (2, 1, 2, 1, '2017-01-01', 150000),
+       (3, 3, 4, 3, '2017-01-01', 2000),
+       (4, 3, 3, 4, '2017-01-01', 30000),
+       (5, 5, 2, 5, '2022-05-20', 10000);
 
 -- Trainiert Table
 INSERT INTO Trainiert (SID, MID, Gehalt)
-VALUES (2, 1, 100000),
-       (1, 2, 90000),
-       (3, 3, 80000),
-       (5, 4, 70000),
-       (4, 5, 60000);
+VALUES (6, 1, 100000),
+       (7, 2, 90000),
+       (8, 3, 80000),
+       (9, 4, 70000),
+       (10, 5, 60000);
 
 -- Gehoertzu Table
 INSERT INTO Gehoertzu (SID, MID, Verletzt)
@@ -163,7 +172,7 @@ VALUES (1, 1),
        (2, 2),
        (3, 3),
        (4, 4),
-       (5, 5);
+       (4, 5);
 
 -- ----------------------------------------------------------------------
 -- Queries
@@ -180,3 +189,96 @@ SELECT * FROM Gehoertzu;
 SELECT * FROM Spielt;
 
 
+SET search_path TO StrengeMettlerKueng;
+
+-- Aufgabe 2.1
+SELECT SID, Vorname, Nachname
+FROM Spieler S
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM StrengeMettlerKueng.Spielt ST
+    WHERE ST.SID = S.SID
+);
+
+-- Aufgabe 2.2
+SELECT SID, Vorname, Nachname AS Typ
+FROM Spieler
+UNION ALL
+SELECT SID, Vorname, Nachname AS Typ
+FROM Trainer;
+
+-- Aufgabe 2.3
+SELECT 
+    S.SID, 
+    S.Vorname, 
+    S.Nachname,
+    CASE 
+        WHEN SUM(T.Abloese) >= 100000 THEN 'teuer'
+        WHEN SUM(T.Abloese) < 100000 AND SUM(T.Abloese) >= 50000 THEN 'normal'
+        ELSE 'billig'
+    END AS Kategorie
+FROM 
+    Transfers T
+JOIN 
+    Spieler S ON T.SID = S.SID
+WHERE 
+    T.Transferdatum BETWEEN '2010-01-01' AND '2018-12-31'
+GROUP BY 
+    S.SID, S.Vorname, S.Nachname
+HAVING 
+    COUNT(T.TID) >= 2;
+	
+-- Aufgabe 2.4
+CREATE OR REPLACE VIEW AverageSalary AS
+SELECT 
+    Position, 
+    Sportart,
+    AVG(Gehalt) AS Durchschnittsgehalt
+FROM 
+    Spieler
+WHERE 
+    Nachname LIKE 'S%' 
+GROUP BY 
+    Position, Sportart
+HAVING 
+    AVG(Gehalt) > 10000;
+SELECT * FROM AverageSalary;
+
+-- Aufgabe 2.5
+WITH SpielerMannschaft AS (
+    SELECT 
+        S.SID,
+        S.Vorname,
+        S.Nachname,
+        M.Name AS Mannschaftsname,
+        M.MID
+    FROM 
+        Spieler S
+    JOIN 
+        Gehoertzu G ON S.SID = G.SID
+    JOIN 
+        Mannschaft M ON G.MID = M.MID
+),
+TrainerMannschaft AS (
+    SELECT 
+        T.SID,
+        T.Vorname,
+        T.Nachname,
+        M.MID
+    FROM 
+        Trainer T
+    JOIN 
+        Trainiert Tr ON T.SID = Tr.SID
+    JOIN 
+        Mannschaft M ON Tr.MID = M.MID
+)
+SELECT 
+    SM.Vorname AS SpielerVorname,
+    SM.Nachname AS SpielerNachname,
+    SM.Mannschaftsname,
+    TM.Vorname AS TrainerVorname,
+    TM.Nachname AS TrainerNachname
+FROM 
+    SpielerMannschaft SM
+LEFT JOIN 
+    TrainerMannschaft TM ON SM.MID = TM.MID;
